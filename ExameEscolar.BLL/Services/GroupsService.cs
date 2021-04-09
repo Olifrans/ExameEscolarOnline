8240b9b7663b6892c1535d3586b70eb87cd468f7
Francisco Oliveira <fransoliveira@gmail.com>
2021-04-08 21:47:59 -0300

    Projeto de  Exame Escolar Online

---
+++ ExameEscolar.BLL/Services/GroupsService.cs
@@ -0,0 +1,44 @@
+﻿using ExameEscolar.DataAccess.UnitOfWork;
+using ExameEscolar.ViewModels;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExameEscolar.BLL.Services
+{
+    public class GroupsService : IGroupsService
+    {
+
+        IUnitOfWork _unitOfWork;
+        ILogger<GroupsService> _ILogger;
+
+        public GroupsService(IUnitOfWork unitOfWork, ILogger<GroupsService> iLogger)
+        {
+            _unitOfWork = unitOfWork;
+            _ILogger = iLogger;
+        }
+
+        public Task<GroupsViewModel> AddGroupsAsync(GroupsViewModel groupVM)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PaginaDeResultado<GroupsViewModel> GetAllGroups(int PaginaNumero, int PaginaTamanho)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GroupsViewModel> GetAllGroups()
+        {
+            throw new NotImplementedException();
+        }
+
+        public GroupsViewModel GetById(int groupId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ExameEscolar.BLL/Services/IGroupsService.cs
@@ -0,0 +1,31 @@
+﻿using ExameEscolar.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExameEscolar.BLL.Services
+{
+    public interface IGroupsService
+    {
+        PaginaDeResultado<GroupsViewModel> GetAllGroups(int PaginaNumero, int PaginaTamanho);
+
+        Task<GroupsViewModel> AddGroupsAsync(GroupsViewModel groupVM);
+
+        IEnumerable<GroupsViewModel> GetAllGroups();
+
+        GroupsViewModel GetById(int groupId);
+
+
+        //bool SetGroupIdToEstudante(GroupsViewModel vm);
+
+        //bool SetExameResultado(AtendeExameViewModel vm);
+
+        //IEnumerable<ResultadoViewModel> GetExameResultado(int estudanteId);
+
+        //EstudanteViewModel GetEstudanteDetalhes(int estudanteId);
+
+        //Task<EstudanteViewModel> UpdateAsync(EstudanteViewModel vm);
+    }
+}
